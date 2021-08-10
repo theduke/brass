365858e043246db7ee01ed5e2f447b406f86919b
Christoph Herzog <chris@theduke.at>
2021-08-10 21:20:41 +0200

    test: Add a test for callbacks

--- tests/web.rs
+++ tests/web.rs
@@ -1,10 +1,23 @@
 use brass::{
     dom::Attr,
-    vdom::{div, Ref},
+    vdom::{self, div, Ref},
+    Callback,
 };
-use wasm_bindgen_test::*;
 
 wasm_bindgen_test::wasm_bindgen_test_configure!(run_in_browser);
+
+fn get_root() -> web_sys::Element {
+    let doc = brass::util::document();
+    if let Some(elem) = doc.get_element_by_id("testapp") {
+        elem.remove();
+    }
+
+    let elem = doc.create_element("div").unwrap();
+    doc.body().unwrap().append_child(&elem).unwrap();
+    elem
+}
+
+// Refs.
 
 struct RefComponent {
     vref: Ref,
@@ -43,19 +56,64 @@ impl brass::Component for RefComponent {
     }
 }
 
-fn get_root() -> web_sys::Element {
-    let doc = brass::util::document();
-    if let Some(elem) = doc.get_element_by_id("testapp") {
-        elem.remove();
-    }
-
-    let elem = doc.create_element("div").unwrap();
-    doc.body().unwrap().append_child(&elem).unwrap();
-    elem
-}
-
-#[wasm_bindgen_test]
-fn refs() {
+#[wasm_bindgen_test::wasm_bindgen_test]
+fn test_refs() {
     tracing_wasm::set_as_global_default();
     brass::boot::<RefComponent>((), get_root());
+}
+
+// Callbacks.
+
+pub struct CallbackComp {
+    count: u64,
+    callback: Callback<u64>,
+}
+
+impl brass::Component for CallbackComp {
+    type Properties = ();
+
+    type Msg = u64;
+
+    fn init(_props: Self::Properties, ctx: &mut brass::Context<Self::Msg>) -> Self {
+        Self {
+            count: 0,
+            callback: ctx.callback(),
+        }
+    }
+
+    fn update(&mut self, msg: Self::Msg, _ctx: &mut brass::Context<Self::Msg>) {
+        self.count += msg;
+    }
+
+    fn render(&self, ctx: brass::RenderContext<Self>) -> brass::VNode {
+        let callback = self.callback.clone();
+        div()
+            .and((
+                div().class("callback-test").and(self.count.to_string()),
+                vdom::button()
+                    .class("callback-test-trigger")
+                    .on_click(ctx.on_opt(move |_ev: web_sys::Event| {
+                        callback.send(1);
+                        None
+                    })),
+            ))
+            .build()
+    }
+
+    fn on_property_change(
+        &mut self,
+        _props: Self::Properties,
+        _ctx: &mut brass::Context<Self::Msg>,
+    ) -> brass::ShouldRender {
+        true
+    }
+}
+
+#[wasm_bindgen_test::wasm_bindgen_test]
+fn test_callback() {
+    brass::boot::<CallbackComp>((), get_root());
+    let btn =
+        brass::util::query_selector_as::<web_sys::HtmlElement>(".callback-test-trigger").unwrap();
+    btn.click();
+    btn.click();
 }
