c5886f4862474ef30972e958acb82083f63fb0bd
Christoph Herzog <chris@theduke.at>
2021-08-15 15:50:01 +0200

    bulma: More static Str usage

--- brass_bulma/src/lib.rs
+++ brass_bulma/src/lib.rs
@@ -8,7 +8,7 @@ pub use dropdown::Dropdown;
 
 use brass::{
     dom::{Attr, Event, Tag},
-    vdom::{self, div, DomExtend, EventCallback, Render, TagBuilder},
+    vdom::{self, div, s, DomExtend, EventCallback, Render, TagBuilder},
     Callback, Str, VNode,
 };
 use vdom::{span, tag};
@@ -39,128 +39,128 @@ impl Color {
 }
 
 pub fn box_() -> TagBuilder {
-    div().class("box")
+    div().class(s("box"))
 }
 
 pub fn navbar_main() -> TagBuilder {
     div()
-        .class("navbar")
-        .attr(Attr::Role, "navigation")
-        .attr(Attr::AriaLabel, "main-navigation")
+        .class(s("navbar"))
+        .attr(Attr::Role, s("navigation"))
+        .attr(Attr::AriaLabel, s("main-navigation"))
 }
 
 pub fn navbar_brand() -> TagBuilder {
-    div().class("navbar-brand")
+    div().class(s("navbar-brand"))
 }
 
 pub fn navbar_menu() -> TagBuilder {
-    div().class("navbar-menu")
+    div().class(s("navbar-menu"))
 }
 
 pub fn navbar_start() -> TagBuilder {
-    div().class("navbar-start")
+    div().class(s("navbar-start"))
 }
 
 pub fn navbar_item_with(content: impl DomExtend) -> TagBuilder {
-    div().class("navbar-item").and(content)
+    div().class(s("navbar-item")).and(content)
 }
 
 // Forms.
 
 pub fn field() -> TagBuilder {
-    div().class("field")
+    div().class(s("field"))
 }
 
 pub fn field_with<C: DomExtend>(content: C) -> TagBuilder {
-    div().class("field").and(content)
+    div().class(s("field")).and(content)
 }
 
 pub fn label() -> TagBuilder {
-    vdom::label().class("label")
+    vdom::label().class(s("label"))
 }
 
 pub fn label_with<C: DomExtend>(content: C) -> TagBuilder {
-    vdom::label().class("label").and(content)
+    vdom::label().class(s("label")).and(content)
 }
 
 pub fn control() -> TagBuilder {
-    div().class("control")
+    div().class(s("control"))
 }
 
 pub fn control_with<C: DomExtend>(content: C) -> TagBuilder {
-    div().class("control").and(content)
+    div().class(s("control")).and(content)
 }
 
 pub fn input() -> TagBuilder {
-    vdom::input().class("input")
+    vdom::input().class(s("input"))
 }
 
 pub fn input_with<C: DomExtend>(content: C) -> TagBuilder {
-    vdom::input().class("input").and(content)
+    vdom::input().class(s("input")).and(content)
 }
 
 pub fn field_help() -> TagBuilder {
-    vdom::p().class("help")
+    vdom::p().class(s("help"))
 }
 
 pub fn field_help_with<C: DomExtend>(content: C) -> TagBuilder {
-    vdom::p().class("help").and(content)
+    vdom::p().class(s("help")).and(content)
 }
 
 pub fn button() -> TagBuilder {
-    vdom::button().class("button")
+    vdom::button().class(s("button"))
 }
 
 pub fn button_small() -> TagBuilder {
-    vdom::button().class("button is-small")
+    vdom::button().class(s("button is-small"))
 }
 
 pub fn button_medium() -> TagBuilder {
-    vdom::button().class("button is-medium")
+    vdom::button().class(s("button is-medium"))
 }
 
 pub fn button_large() -> TagBuilder {
-    vdom::button().class("button is-large")
+    vdom::button().class(s("button is-large"))
 }
 
 pub fn buttons() -> TagBuilder {
-    vdom::div().class("buttons")
+    vdom::div().class(s("buttons"))
 }
 
 pub fn h2() -> TagBuilder {
-    vdom::h2().class("title is-2")
+    vdom::h2().class(s("title is-2"))
 }
 
 pub fn h2_with<C: DomExtend>(content: C) -> TagBuilder {
-    vdom::h2().class("title is-2").and(content)
+    vdom::h2().class(s("title is-2")).and(content)
 }
 
 pub fn h3() -> TagBuilder {
-    vdom::h3().class("title is-3")
+    vdom::h3().class(s("title is-3"))
 }
 
 pub fn h3_with<C: DomExtend>(content: C) -> TagBuilder {
-    vdom::h3().class("title is-3").and(content)
+    vdom::h3().class(s("title is-3")).and(content)
 }
 
 pub fn h4() -> TagBuilder {
-    vdom::h4().class("title is-4")
+    vdom::h4().class(s("title is-4"))
 }
 
 pub fn h4_with<C: DomExtend>(content: C) -> TagBuilder {
-    vdom::h4().class("title is-4").and(content)
+    vdom::h4().class(s("title is-4")).and(content)
 }
 
 pub fn h5() -> TagBuilder {
-    vdom::h5().class("title is-5")
+    vdom::h5().class(s("title is-5"))
 }
 
 pub fn h5_with<C: DomExtend>(content: C) -> TagBuilder {
-    vdom::h5().class("title is-5").and(content)
+    vdom::h5().class(s("title is-5")).and(content)
 }
 
 pub fn menu_list() -> TagBuilder {
-    vdom::ul().class("menu-list")
+    vdom::ul().class(s("menu-list"))
 }
 
 pub fn notification(color: Color, content: impl DomExtend) -> TagBuilder {
@@ -181,12 +181,12 @@ pub fn notification_error(content: impl DomExtend) -> TagBuilder {
 
 #[inline]
 pub fn table() -> TagBuilder {
-    vdom::table().class("table")
+    vdom::table().class(s("table"))
 }
 
 pub fn icon_fa(icon: impl Into<Str>) -> TagBuilder {
     span()
-        .class("icon")
+        .class(s("icon"))
         .attr(Attr::AriaHidden, "true")
         .and(tag(Tag::I).class(icon))
 }
@@ -194,73 +194,77 @@ pub fn icon_fa(icon: impl Into<Str>) -> TagBuilder {
 pub fn modal<C: DomExtend>(content: C, on_close: Callback<()>) -> TagBuilder {
     let on_close_ev = EventCallback::callback(|_| (), on_close);
     let bg = div()
-        .class("modal-background")
+        .class(s("modal-background"))
         .on(Event::Click, on_close_ev.clone());
 
-    let inner = div().class("modal-content").and(content);
+    let inner = div().class(s("modal-content")).and(content);
 
     let close = button()
-        .class("modal-close is-large")
+        .class(s("modal-close is-large"))
         .attr(Attr::AriaLabel, "close")
         .on(Event::Click, on_close_ev.clone());
 
-    div().class("modal is-active").and(bg).and(inner).and(close)
+    div()
+        .class(s("modal is-active"))
+        .and(bg)
+        .and(inner)
+        .and(close)
 }
 
 pub fn file_input(label: &str, on_input: EventCallback, disabled: bool, multi: bool) -> TagBuilder {
     let input = vdom::input()
-        .class("file-input")
+        .class(s("file-input"))
         .attr(Attr::Type, "file")
         .attr_if(disabled, Attr::Disabled, "")
         .attr_if(multi, Attr::Multiple, "")
         .on(Event::Input, on_input);
     let icon = span()
-        .class("file-icon")
-        .and(vdom::tag(Tag::I).class("fas fa-upload"));
-    let inner_label = span().class("file-label").and(label);
+        .class(s("file-icon"))
+        .and(vdom::tag(Tag::I).class(s("fas fa-upload")));
+    let inner_label = span().class(s("file-label")).and(label);
 
-    let extra = span().class("file-cta").and(icon).and(inner_label);
+    let extra = span().class(s("file-cta")).and(icon).and(inner_label);
 
-    let label = vdom::label().class("file-label").and(input).and(extra);
-    div().class("file").and(label)
+    let label = vdom::label().class(s("file-label")).and(input).and(extra);
+    div().class(s("file")).and(label)
 }
 
 // Panels.
 
 pub fn panel() -> TagBuilder {
-    vdom::tag(Tag::Nav).class("panel")
+    vdom::tag(Tag::Nav).class(s("panel"))
 }
 
 pub fn panel_heading<C: DomExtend>(content: C) -> TagBuilder {
-    vdom::p_with(content).class("panel-heading")
+    vdom::p_with(content).class(s("panel-heading"))
 }
 
 pub fn panel_block() -> TagBuilder {
-    div().class("panel-block")
+    div().class(s("panel-block"))
 }
 
 pub fn panel_icon_fa(icon: &str) -> TagBuilder {
     span()
-        .class("panel-icon")
+        .class(s("panel-icon"))
         .and(vdom::tag(Tag::I).class(icon))
 }
 
 // Cards.
 
 pub fn card() -> TagBuilder {
-    div().class("card")
+    div().class(s("card"))
 }
 
 pub fn card_header() -> TagBuilder {
-    vdom::header().class("card-header")
+    vdom::header().class(s("card-header"))
 }
 
 pub fn card_header_title(content: impl DomExtend) -> TagBuilder {
-    vdom::p().class("card-header-title").and(content)
+    vdom::p().class(s("card-header-title")).and(content)
 }
 
 pub fn card_content() -> TagBuilder {
-    div().class("card-content")
+    div().class(s("card-content"))
 }
 
 #[derive(Debug)]
@@ -323,11 +327,11 @@ impl<C: Render> Render for FieldHorizontal<C> {
             .and_class("is-horizontal")
             .and((
                 div()
-                    .class("field-label is-normal")
+                    .class(s("field-label is-normal"))
                     .and(label_with(self.label)),
-                div().class("field-body").and(
+                div().class(s("field-body")).and(
                     field()
-                        .and(div().class("control").and(self.control))
+                        .and(div().class(s("control")).and(self.control))
                         .and_opt(self.help),
                 ),
             ))
@@ -469,7 +473,7 @@ impl<'a, T: PartialEq + Eq + Clone + std::hash::Hash> Render for TagSelect<'a, T
                 })))
         });
 
-        div().class("tags").and_iter(options).build()
+        div().class(s("tags")).and_iter(options).build()
     }
 }
 
@@ -482,18 +486,18 @@ pub struct FileInput {
 impl DomExtend for FileInput {
     fn extend(self, parent: &mut TagBuilder) {
         let input = vdom::input()
-            .class("file-input")
+            .class(s("file-input"))
             .attr(Attr::Type, "file")
             .attr_toggle_if(self.multi, Attr::Multiple)
             .on(Event::Change, self.on_change);
-        let cta = vdom::span().class("file-cta").and((
+        let cta = vdom::span().class(s("file-cta")).and((
             vdom::span()
-                .class("file-icon")
-                .and(vdom::i().class("fas fa-upload")),
-            vdom::span().class("file-label").and(self.label),
+                .class(s("file-icon"))
+                .and(vdom::i().class(s("fas fa-upload"))),
+            vdom::span().class(s("file-label")).and(self.label),
         ));
-        let label = vdom::label().class("file-label").and((input, cta));
-        let n = div().class("file").and(label);
+        let label = vdom::label().class(s("file-label")).and((input, cta));
+        let n = div().class(s("file")).and(label);
 
         parent.add_child(n);
     }
@@ -511,7 +515,7 @@ impl Checkbox {
         let inp = vdom::input()
             .attr(Attr::Type, "checkbox")
             .on(Event::Input, self.on_input);
-        let lbl = vdom::label().class("checkbox").and(inp).and(self.label);
+        let lbl = vdom::label().class(s("checkbox")).and(inp).and(self.label);
         let ctrl = control().and(lbl);
         field().and(ctrl)
     }
