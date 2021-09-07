b99734d5a58c398760fa8b6bc575c89c345d01f9
Christoph Herzog <chris@theduke.at>
2021-09-07 15:52:16 +0200

    refactor: Use helper macro for string enums (Event,Tag,Attr)
    Introduce a make_str_enum! helper macro for creating enums with attached
    string values.
    
    Also convert the Event,Tag and Attr enums to the macro.

--- src/dom/mod.rs
+++ src/dom/mod.rs
@@ -1,8 +1,60 @@
+#[macro_export]
+macro_rules! make_str_enum {
+    (
+        $enum_name:ident {
+            $( $name:ident = $num:literal = $value:literal, )*
+            ;
+            $last_name:ident = $last_num:literal = $last_value:literal
+        }
+    ) => {
+        #[repr(u16)]
+        #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
+        pub enum $enum_name {
+            $( $name = $num, )*
+            $last_name = $last_num,
+        }
+
+        impl $enum_name {
+            /// The number of [`Attr`] variants.
+            ///
+            /// Useful for things like lookup arrays.
+            pub(crate) const fn variant_count() -> usize {
+                $last_num + 1
+            }
+
+            /// Convert to numeric representation.
+            pub(crate) const fn as_u16(self) -> u16 {
+                self as u16
+            }
+
+            pub(crate) const fn from_u16(value: u16) -> Option<Self> {
+                match value {
+                    $(
+                        $num => Some(Self::$name),
+                    )*
+                    $last_num => Some(Self::$last_name),
+                    _ => None,
+                }
+            }
+
+            pub fn as_str(&self) -> &str {
+                match self {
+                    $(
+                        Self::$name => $value,
+                    )*
+                    Self::$last_name => $last_value,
+                    // Self::Custom(value) => value.as_ref(),
+                }
+            }
+        }
+    };
+}
+
 mod event;
 pub use event::*;
 
 mod tag;
-pub use tag::*;
+pub use tag::Tag;
 
 mod attribute;
-pub use attribute::*;
+pub use attribute::Attr;
