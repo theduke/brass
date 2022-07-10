3c8d5f2a819ea78a71adf8a7f3813e7cce3a390d
Christoph Herzog <chris@theduke.at>
2022-07-10 10:22:53 +0200

    Add Style::MaxHeight

--- brass/src/dom/style.rs
+++ brass/src/dom/style.rs
@@ -4,6 +4,7 @@ make_str_enum! {
         Display = "display",
         Color = "color",
         Height = "height",
+        MaxHeight = "max-height",
         Width = "width",
         Background = "background",
         BackgroundColor = "background-color",
